5fff43dd1605d5d9f4a669f4762c1f16dfd5d1ad
clement.piat2 <clement.piat2@gmail.com>
2024-01-06 19:05:16 +0100

    fix: manifest

--- src/app/components/SavedWords.jsx
+++ src/app/components/SavedWords.jsx
@@ -16,12 +16,12 @@ export default function SavedWords(props) {
     <main className="flex h-[100vh] flex-col items-center justify-between custom-bg">
       <div className="w-[70vw] items-center">
         <Definition selectedWord={props.definitions[selectedWord]}></Definition>
-        <ul className="text-slate-100 fixed overflow-y-auto top-[40vh] sm:top-[40vh] left-[15vw] sm:left-[15vw] w-[70vw] sm:w-[20vw] bg-slate-900 rounded-lg text-slate-100 max-h-[45vh] pl-[2vh] pb-[1vh]">
+        <ul className="text-slate-100 fixed overflow-y-auto top-[40vh] sm:top-[40vh] left-[15vw] sm:left-[15vw] w-[70vw] sm:w-[20vw] bg-slate-900 rounded-lg text-slate-100 max-h-[45vh] pl-[2vh] pt-[2vh] pb-[1vh]">
             {
                 savedWords.map(
                     el => 
-                    <li key={el} className="mt-[1vh]">
-                        <span className="hover:text-pink-700 cursor-help" onMouseEnter={() => setSelectedWord(el)}>{el}</span>
+                    <li key={el} className="mb-[1vh]">
+                        <span className="hover:font-extrabold cursor-help" onMouseEnter={() => setSelectedWord(el)}>{el}</span>
                     </li>
                 )
             }
