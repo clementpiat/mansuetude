21e6bebab461912e541c4e32189cbcaec0eb9c34
clement.piat2 <clement.piat2@gmail.com>
2024-02-12 10:29:33 +0100

    fix: back to prev data

--- src/app/components/ScrollLandingPage.jsx
+++ src/app/components/ScrollLandingPage.jsx
@@ -142,7 +142,6 @@ export default function ScrollLandingPage(props) {
       <div id="title" className="w-[70vw] h-[60vh] pt-[20vh] sm:pt-[25vh] items-center mb-[15vh]">
         <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight text-slate-50">
           <span>Apprentissage de vocabulaire </span>
-          <div id="highlight"></div>
           <span>français </span>
           <span>avancé</span>
         </h1>
